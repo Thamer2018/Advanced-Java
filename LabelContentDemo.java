fb98a51fb43df979b8c35327ec598065160631ea
Thameur Aissaoui <thameur.aissaoui@gmail.com>
2018-05-30 16:57:20 -0400

    Simple Drag and Drop Example

---
+++ LabelContentDemo.java
@@ -0,0 +1,71 @@
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.TransferHandler;
+
+//CTRL + SHIFT + O pour générer les imports
+/**
+ * @author thame
+ *
+ */
+public class LabelContentDemo extends JFrame {
+
+	/**
+	 * 
+	 */
+	public LabelContentDemo() {
+		setTitle("Drag'n Drop avec un JLabel !");
+		setSize(300, 100);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JPanel pan = new JPanel();
+		pan.setLayout(new GridLayout(2, 2));
+		pan.setBackground(Color.white);
+
+		JLabel srcLib = new JLabel("Source de drag : ", JLabel.RIGHT);
+		JLabel src = new JLabel("Texte à déplacer !");
+
+		// --------------------------------------------------------
+		// On utilise notre nouvel objet MyTransferHandle
+		src.setTransferHandler(new MyTransferHandler());
+
+		src.addMouseListener(new MouseAdapter() {
+
+			public void mousePressed(MouseEvent e) {
+				System.out.println("EVENT !");
+				JComponent lab = (JComponent) e.getSource();
+				TransferHandler handle = lab.getTransferHandler();
+				handle.exportAsDrag(lab, e, TransferHandler.COPY);
+			}
+		});
+		// --------------------------------------------------------
+
+		JLabel destLib = new JLabel("Destination de drag : ", JLabel.RIGHT);
+		JTextField dest = new JTextField();
+
+		dest.setDragEnabled(true);
+
+		pan.add(srcLib);
+		pan.add(src);
+		pan.add(destLib);
+		pan.add(dest);
+
+		setContentPane(pan);
+		setVisible(true);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new LabelContentDemo();
+	}
+}
